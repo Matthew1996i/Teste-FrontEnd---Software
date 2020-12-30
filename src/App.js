356b5395fe94a1d7420ab5068ecce17f27c7f59e
matthew1996i <matthew199624@gmail.com>
2020-12-30 18:33:22 -0300

    components separated by folders were created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import "./App.css";
+
+const App = () => {
+  return(
+    <React.Fragment>
+      <Header/>
+      <Main/>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Main = () => {
+  return(
+    <div>
+      <div className="listHeader">
+        <ul>
+          <li>title item</li>
+          <li>title item</li>
+          <li>title item</li>
+          <li>title item</li>
+        </ul>
+      </div>
+      <div className="listContent">
+        <ul>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default Main
